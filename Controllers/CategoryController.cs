a1a3e9add3665ad365b400dbde44759f2b253713
asith ishantha <you@example.com>
2020-07-10 19:27:08 +0530

    My first File

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoneyManager.Models;
+
+namespace MoneyManager.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            Category categery = db.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+            db.Dispose();
+            return View(categery);
+        }
+
+        public ActionResult Details(int id)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            Category categery = db.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+            db.Dispose();
+            return View(categery);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            Category categery = db.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+            db.Dispose();
+            return View(categery);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Add(Category categery)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            db.Categories.Add(categery);
+            db.SaveChanges();
+            db.Dispose();
+            return Redirect("Index");
+        }
+
+        public ActionResult Save(Category categoryEdited)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            Category category = db.Categories.Where(x => x.CategoryId == categoryEdited.CategoryId).FirstOrDefault();
+            category.CategoryName = categoryEdited.CategoryName;
+            db.SaveChanges();
+            db.Dispose();
+            return Redirect("Index");
+        }
+
+        public ActionResult Remove(Category categoryEdited)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            Category category = db.Categories.Where(x => x.CategoryId == categoryEdited.CategoryId).FirstOrDefault();
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            db.Dispose();
+            return Redirect("Index");
+        }
+    }
+}
---
+++ Controllers/MoneyController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoneyManager.Models;
+
+namespace MoneyManager.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult Index()
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            List<view_Expense> expences = db.view_Expense.ToList();
+            db.Dispose();
+            return View(expences);
+        }
+        public ActionResult Details(int id)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            view_Expense expences2 = db.view_Expense.Where(x => x.ExpenseId == id).FirstOrDefault();
+            db.Dispose();
+            return View(expences2);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            view_Expense expences2 = db.view_Expense.Where(x => x.ExpenseId == id).FirstOrDefault();
+            db.Dispose();
+            return View(expences2);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            view_Expense expences2 = db.view_Expense.Where(x => x.ExpenseId == id).FirstOrDefault();
+            db.Dispose();
+            return View(expences2);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Update(view_Expense UpdatedExpenses)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            Expense expenses = db.Expenses.Where(x => x.ExpenseId == UpdatedExpenses.ExpenseId).FirstOrDefault();
+            expenses.Description = UpdatedExpenses.Description;
+            expenses.Date = UpdatedExpenses.Date;
+            expenses.Amount = UpdatedExpenses.Amount;
+
+            Category category = db.Categories.Where(x => x.CategoryName == UpdatedExpenses.CategoryName).FirstOrDefault();
+            if (category == null)
+            {
+                Category new_category = new Category();
+                new_category.CategoryName = UpdatedExpenses.CategoryName;
+                db.Categories.Add(new_category);
+                db.SaveChanges();
+                category = db.Categories.Where(x => x.CategoryName == UpdatedExpenses.CategoryName).FirstOrDefault();
+            }
+
+            expenses.CategoryId = category.CategoryId;
+
+            db.SaveChanges();
+
+            db.Dispose();
+            return Redirect("Index");
+        }
+
+        public ActionResult Add(view_Expense UpdatedExpenses)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            Expense expenses = new Expense();
+            expenses.Description = UpdatedExpenses.Description;
+            expenses.Date = UpdatedExpenses.Date;
+            expenses.Amount = UpdatedExpenses.Amount;
+
+            Category category = db.Categories.Where(x => x.CategoryName == UpdatedExpenses.CategoryName).FirstOrDefault();
+            if (category == null)
+            {
+                Category new_category = new Category();
+                new_category.CategoryName = UpdatedExpenses.CategoryName;
+                db.Categories.Add(new_category);
+                db.SaveChanges();
+                category = db.Categories.Where(x => x.CategoryName == UpdatedExpenses.CategoryName).FirstOrDefault();
+            }
+
+            expenses.CategoryId = category.CategoryId;
+            db.Expenses.Add(expenses);
+            db.SaveChanges();
+
+            db.Dispose();
+            return Redirect("Index");
+        }
+
+
+        public ActionResult Remove(view_Expense UpdatedExpenses)
+        {
+            AssignmentEntities db = new AssignmentEntities();
+            Expense expences = db.Expenses.Where(x => x.ExpenseId == UpdatedExpenses.ExpenseId).FirstOrDefault();
+            db.Expenses.Remove(expences);
+            db.SaveChanges();
+            db.Dispose();
+            return Redirect("Index");
+        }
+
+    }
+}
